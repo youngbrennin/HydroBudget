41745528b735de3691a2ef6deb12b9ef16bb397b
JustMatt <MatthewBertrandt@gmail.com>
2018-06-18 18:53:48 -0700

    more test changes

---
+++ assets/javascript/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    console.log(chance.d20());
+
+
+
+
+    // ID: name car [{comments}] like-dislike ratio
+
+    //walk in 
+    //assign location to a key
+    //load up a comment history in that key
+});
