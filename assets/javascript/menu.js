8ce415efb1c4344c20c033885bbd9b0a3727635e
Bob Ross <ministash14@gmail.com>
2018-06-25 00:58:34 -0700

    Dramatically changed the entire css for the page

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,12 +1,12 @@
-$(".edit-button").on("click", function () {
+$(".monthlyEdit").on("click", function () {
     $(".textId").html("");
     $(".textId").append(
-        '<input id="userInput2" type="number" placeholder="Amount" value="" />'
+        '<input id="userinput2" type="number" placeholder="Amount" value="">'
     );
 })
 
-$('.submit-button').on('click', function () {
-   var moneyInformation = $('#userInput2').val().trim();
+$('.monthlySubmit').on('click', function () {
+   let moneyInformation = $('#userInput2').val().trim();
    $(".textId").html("$" + moneyInformation);
 
 })
