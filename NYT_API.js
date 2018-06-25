237c108dc2ad6b267285877b4b2477ab0e4b7829
Michelangelo <youngbrennin20@gmail.com>
2018-06-24 20:15:59 -0700

    added link to display on another page and deleted random non important stuff

--- NYT_API.js
+++ NYT_API.js
@@ -10,29 +10,18 @@ $.ajax({
 
 
   function updatePage(NYTData) {
-    // Get from the form the number of results to display
-    // API doesn't have a "limit" parameter, so we have to do this ourselves
+   
     var numArticles = 1;
-  
-    // Log the NYTData to console, where it will show up as an object
     console.log(NYTData);
     console.log("------------------------------------");
   
-    // Loop through and build elements for the defined number of articles
-      // Get specific article info for current index
       var article = NYTData.results[0];
-  
-      // Increase the articleCount (track article # - starting at 1)
       
-  
-      // Create the  list group to contain the articles and add the article content for each
-      var $articleList = $("<p>");
+        var $articleList = $("<p>");
       $articleList.addClass("list-group");
   
-      // Add the newly created element to the DOM
       $("#NYT").append($articleList);
   
-      // If the article has a headline, log and append to $articleList
       var headline = article.headline;
       var $articleListItem = $("<p class='list-group-item articleHeadline'>");
   
@@ -48,7 +37,6 @@ $.ajax({
         );
       }
   
-      // If the article has a byline, log and append to $articleList
       var byline = article.byline;
   
       if (byline && byline.original) {
@@ -56,7 +44,6 @@ $.ajax({
         $articleListItem.append("<h5>" + byline.original + "</h5>");
       }
   
-      // Log section, and append to document if exists
       var section = article.abstract;
       var titleName = article.title;
       var linkName = article.url;
@@ -66,49 +53,9 @@ $.ajax({
       if (section) {
         $articleListItem.append("<h5>" + titleName + "</h5>");
         $articleListItem.append("<h6>" + section + "</h6>");
-        $articleListItem.append("<h6><a href='https://www.nytimes.com/2018/06/22/your-money/fiduciary-rule-dies.html'>" + linkName + "</a></h6>");
+        $articleListItem.append("<h6><a href='https://www.nytimes.com/2018/06/22/your-money/fiduciary-rule-dies.html' target=_blank>" + linkName + "</a></h6>");
       }
   
-      // Log published date, and append to document if exists
-      
-  
-      // Append and log url
-      
-  
-      // Append the article
       $articleList.append($articleListItem);
     
   }
-  
-  // Function to empty out the articles
-//   function clear() {
-//     $("#article-section").empty();
-//   }
-  
-//   // CLICK HANDLERS
-//   // ==========================================================
-  
-//   // .on("click") function associated with the Search Button
-//   $("#run-search").on("click", function(event) {
-//     // This line allows us to take advantage of the HTML "submit" property
-//     // This way we can hit enter on the keyboard and it registers the search
-//     // (in addition to clicks). Prevents the page from reloading on form submit.
-//     event.preventDefault();
-  
-//     // Empty the region associated with the articles
-//     clear();
-  
-//     // Build the query URL for the ajax request to the NYT API
-//     var queryURL = buildQueryURL();
-  
-//     // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-//     // The data then gets passed as an argument to the updatePage function
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(updatePage);
-//   });
-  
-//   //  .on("click") function associated with the clear button
-//   $("#clear-all").on("click", clear);
-  
