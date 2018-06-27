2142b4d28ede1d3fee49998b8730bd6ba9c0353c
Michelangelo <youngbrennin20@gmail.com>
2018-06-27 02:19:23 -0700

    made style mods to chart

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -3,8 +3,8 @@ var userID, userRef, currentPage, accountInfo, billList = [];
 var pieChartData = {
     items: [['test', 1], ['test2', 2], ['test2', 2], ['test2', 2], ['test2', 2], ['test2', 2], ['test2', 2], ['test2', 2], ['test2', 2]],
     title: 'test title please ignore',
-    width: '',
-    height: '',
+    width: '400',
+    height: '250',
     div: 'test'
 };
 //setters
@@ -41,9 +41,13 @@ function drawPieChart() {
     // Set chart options
     var options = {
         'title': pieChartData.title,
-        'width': pieChartData.width,
-        'height': pieChartData.height
-    };
+        'width': '610',
+        'height': '250',
+        'colors': ['#3da4ab', '#f6cd61', '#fe8a71', '#4a4e4d', '#1ccedf', '#f85b37', '#79807e', '#fadb8c', '#00e7fc', '#ffb700', '#6f8e96'],
+        'backgroundColor' : '#818586',
+        'titleTextStyle' : { color: 'white', bold: true},
+        'legend' : {textStyle: {color: 'white'}},
+        };
 
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById(pieChartData.div));
@@ -53,12 +57,12 @@ function drawPieChart() {
 var WebPages = [
     //use &#39 for '
     // newPage('Welcome to Hydro...???', 'This is your first time here so we will guide you through this'),
-    /* page 1 */newPage('<div id="mainStarterBox"> <p>Welcome to Hyrdo Budget! Your best source for simply saving money based on your expenses and budget. Click the button below to begin! </p><button id="startButton" class="x next-page-button">Get Started!</button> </div>'),
-    /* page 2 */newPage('<div class="container" id="mainStarterBox"> <p class="x">What is your average <a id="toolTipButton" class="tooltipped x" data-position="top" data-tooltip="Net income is the amount of money an individual makes after the usual deductions from a paycheck, such as social security, 401k, taxes, etc...">net</a> income per month?</p> <form> <input id="userInput" type="text" placeholder="Amount" value="" /> </form> <div id="startButton" class="x submit-income next-page-button">Submit</div> </div> </div>'),
+    /* page 1 */newPage('<nav> <div class="nav-wrapper" id="navBar"> <img src="assets&#92images&#92waterdrop.png" id="logo"> <img src="assets&#92images&#92hydro.png" id="logo2"> <a class="brand-logo center"> <i class="material-icons" style="color: #fe8a71;"></i> </a> </nav><div id="mainStarterBox"> <p>Welcome to Hyrdo Budget! Your best source for simply saving money based on your expenses and budget. Click the button below to begin! </p><button id="startButton" class="x next-page-button">Get Started!</button> </div>'),
+    /* page 2 */newPage('<nav> <div class="nav-wrapper" id="navBar"> <img src="assets&#92images&#92waterdrop.png" id="logo"> <img src="assets&#92images&#92hydro.png" id="logo2"> <a class="brand-logo center"> <i class="material-icons" style="color: #fe8a71;"></i> </a> </nav><div class="container" id="mainStarterBox"> <p class="x">What is your average <a id="toolTipButton" class="tooltipped x" data-position="top" data-tooltip="Net income is the amount of money an individual makes after the usual deductions from a paycheck, such as social security, 401k, taxes, etc...">net</a> income per month?</p> <form> <input id="userInput" type="text" placeholder="Amount" value="" /> </form> <div id="startButton" class="x submit-income next-page-button">Submit</div> </div> </div>'),
     //etc...
-    newPage('<div class="container" id="mainStarterBox2"> <p>Starting off with your bills, let&#39s begin with your expenses that are reoccuring on a monthly basis. <a id="toolTipButton" class="tooltipped x" data-position="top" data-tooltip="Don&#39t worry, you can add/edit/remove details to this section later on">*</a> Click on the yellow box to to enter a date, and the add button to create a new expense on the list below.</p> <div class="rowWrapper3 z-depth-3"> <div class="topRowWrapper3"> <div class="firstItemDescription3"> DATE </div> <div class="itemDescription3"> NAME </div> <div class="itemDescription3"> AMOUNT </div> </div> <div class="newBill bh"> <input id="billDateInputTopPage" class="datepicker" placeholder="Date" value=""> <input id="billNameInputTopPage" type="number/text" placeholder="Bill Name" value=""> <input id="billAmountInputTopPage" type="number" placeholder="Amount" value=""> </div> </div> <div class="x add submit-new-bill" id="startButton2">Add</div> <div class="rowWrapper1 z-depth-3"> <div class="topRowWrapper"> <div id="descriptionDate"> DATE </div> <div class="itemDescription"> NAME </div> <div class="itemDescription"> AMOUNT </div> </div> <div id="bill-list"></div> </div> <div class="x add next-page-button" id="startButton2">Submit</div>'),
+    newPage('<nav> <div class="nav-wrapper" id="navBar"> <img src="assets&#92images&#92waterdrop.png" id="logo"> <img src="assets&#92images&#92hydro.png" id="logo2"> <a class="brand-logo center"> <i class="material-icons" style="color: #fe8a71;"></i> </a> </nav><div class="container" id="mainStarterBox2"> <p>Starting off with your bills, let&#39s begin with your expenses that are reoccuring on a monthly basis. <a id="toolTipButton" class="tooltipped x" data-position="top" data-tooltip="Don&#39t worry, you can add/edit/remove details to this section later on">*</a> Click on the yellow box to to enter a date, and the add button to create a new expense on the list below.</p> <div class="rowWrapper3 z-depth-3"> <div class="topRowWrapper3"> <div class="firstItemDescription3"> DATE </div> <div class="itemDescription3"> NAME </div> <div class="itemDescription3"> AMOUNT </div> </div> <div class="newBill bh"> <input id="billDateInputTopPage" class="datepicker" placeholder="Date" value=""> <input id="billNameInputTopPage" type="number/text" placeholder="Bill Name" value=""> <input id="billAmountInputTopPage" type="number" placeholder="Amount" value=""> </div> </div> <div class="x add submit-new-bill" id="startButton2">Add</div> <div class="rowWrapper1 z-depth-3"> <div class="topRowWrapper"> <div id="descriptionDate"> DATE </div> <div class="itemDescription"> NAME </div> <div class="itemDescription"> AMOUNT </div> </div> <div id="bill-list"></div> </div> <div class="x add next-page-button" id="startButton2">Submit</div>'),
 
-    newPage('<div class="row"> <div id="leftSide" class="col s5"> <div id="netIncome" class="z-depth-3"> Net Monthly Salary <div class="newBill bh z-depth-3"> <div class="month"> Monthly:</div> <div class="textId">$0</div> <button id="salaryEdit" class="edit-button-2 b bh monthlyEdit">✎</button> <button id="salarySubmit" class="submit-button-2 b bh monthlySubmit">✓</button> </div> </div> <div id="totalExpenses" class="z-depth-3"> Total Expenses <div id="totalExpensesDisplayed"> $0 </div> </div> <div id="NYThead" class="z-depth-3"> Latest Money Related News <div id="NYT"> </div> </div> <div id="chart"></div></div> <!-- Everything on the right side of the page--> <!-- THE GREAT PAGE DIVIDE --> <div id="rightSide" class="col s7"> <div id="allTheBills" class="z-depth-3 dateWrapper"> <div>Add A New Bill!</div> <div class="rowWrapper3 z-depth-3"> <div class="topRowWrapper3"> <div class="firstItemDescription3"> DATE </div> <div class="itemDescription3"> NAME </div> <div class="itemDescription3"> AMOUNT </div> </div> <div class="newBill bh z-depth-3"> <input id="billDateInputTopPage" class="datepicker" placeholder="Date" value=""> <input id="billNameInputTopPage" type="number/text" placeholder="Bill Name" value=""> <input id="billAmountInputTopPage" type="number" placeholder="Amount" value=""> </div> </div> <div class="x add submit-new-bill" id="startButton2">Add</div> </div> <div class="rowWrapper2 z-depth-3"> <div class="billList">Bill List</div> <div class="topRowWrapper"> <div id="descriptionDate"> DATE </div> <div class="itemDescription"> NAME </div> <div class="itemDescription"> AMOUNT </div> </div> <div id="bill-list"></div> </div> </div> <!-- Everything on the left side of the page--> </div>'),
+    newPage('<nav> <div class="nav-wrapper" id="navBar"> <img src="assets&#92images&#92waterdrop.png" id="logo"> <img src="assets&#92images&#92hydro.png" id="logo2"> <div class="center" id="savings">Savings: $0 </div> </nav><div class="row"> <div id="leftSide" class="col s5"> <div id="netIncome" class="z-depth-3"> Net Monthly Salary <div class="newBill bh z-depth-3"> <div class="month"> Monthly:</div> <div class="textId">$0</div> <button id="salaryEdit" class="edit-button-2 b bh monthlyEdit">✎</button> <button id="salarySubmit" class="submit-button-2 b bh monthlySubmit">✓</button> </div> </div> <div id="totalExpenses" class="z-depth-3"> Total Expenses <div id="totalExpensesDisplayed"> $0 </div> </div> <div id="chart"></div> <div id="NYThead" class="z-depth-3"> Latest Money Related News <div id="NYT"> </div> </div></div> <!-- Everything on the right side of the page--> <!-- THE GREAT PAGE DIVIDE --> <div id="rightSide" class="col s7"> <div id="allTheBills" class="z-depth-3 dateWrapper"> <div>Add A New Bill!</div> <div class="rowWrapper3 z-depth-3"> <div class="topRowWrapper3"> <div class="firstItemDescription3"> DATE </div> <div class="itemDescription3"> NAME </div> <div class="itemDescription3"> AMOUNT </div> </div> <div class="newBill bh z-depth-3"> <input id="billDateInputTopPage" class="datepicker" placeholder="Date" value=""> <input id="billNameInputTopPage" type="number/text" placeholder="Bill Name" value=""> <input id="billAmountInputTopPage" type="number" placeholder="Amount" value=""> </div> </div> <div class="x add submit-new-bill" id="startButton2">Add</div> </div> <div class="rowWrapper2 z-depth-3"> <div class="billList">Bill List</div> <div class="topRowWrapper"> <div id="descriptionDate"> DATE </div> <div class="itemDescription"> NAME </div> <div class="itemDescription"> AMOUNT </div> </div> <div id="bill-list"></div> </div> </div> <!-- Everything on the left side of the page--> </div>'),
 
     //some kinda loading page
     newPage("<div class='container' id='mainStarterBox'>Hey! I'm loading here!</div>")
@@ -77,7 +81,7 @@ function newPage(content) {
         },
         display: function () {
             currentPage = this;
-            var thisPage = $("#content");
+            var thisPage = $("body");
             thisPage.hide().empty();
             var thisIndex = WebPages.indexOf(this);
 
@@ -95,9 +99,8 @@ function newPage(content) {
                 case WebPages.length - 2: {
                     updateTotalExpensesDiv();
                     updateSalaryDiv();
-                    // displayBills();
-                    displayBillsThisMonth();
-                    displayNews();
+                    displayBills();
+                    displayNews(accountInfo.salary - accountInfo.bill_total);
                     var graphed_bills = [];
                     accountInfo.bills.forEach(function (e) {
                         graphed_bills.push([e.name, e.amount_budgeted]);
@@ -243,11 +246,11 @@ function swap(data, a, b) {
 }
 function updateTotalExpensesDiv() {
     var total_expenses = numeral(accountInfo.budgeted_bill_total);
-    $('#totalExpensesDisplayed').text(total_expenses.format('$0,'));
+    $('#totalExpensesDisplayed').text(total_expenses.format('$0,.00'));
 }
 function updateSalaryDiv() {
     var salary = numeral(accountInfo.salary);
-    $('.textId').text(salary.format('$0,'));
+    $('.textId').text(salary.format('$0,.00'));
 }
 function savingsRound(num) {
     var newSave = Math.ceil((num * .15));
@@ -259,7 +262,7 @@ function savingsRound(num) {
     return ret;
 }
 function spacesToUnderscore(str) {
-    var ret = str.split(' ').join('_');
+    var ret = str.split(' ').join('-');
     return ret;
 }
 $(document).ready(function () {
@@ -384,7 +387,7 @@ $(document).ready(function () {
             amount: amount,
             amount_budgeted: amount_budgeted,
             amount_saved: amount_budgeted - amount,
-            date: $("#billDateInputTopPage").val().trim()
+            date: moment($("#billDateInputTopPage").val().trim(), 'MMM DD, YYYY').format('Do')
         }
 
         for (var i = 0; i < accountInfo.bills.length; i++) {
